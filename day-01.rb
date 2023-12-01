991f09be25fa361bdb707b8b49ca301f7dce20aa
Jérémie <jeremie.bonal@gmail.com>
2023-12-01 10:44:17 +0100

    Part 1

--- day-01.rb
+++ day-01.rb
@@ -1,7 +1,15 @@
 require_relative 'common'
 
 class Day1 < AdventDay
+  EXPECTED_RESULTS = { 1 => 142, 2 => nil }
+
   def first_part
+    calibration_values = input.map do |line|
+      digits = line.gsub(/\D/, '').chars
+      [digits.first, digits.last].join.to_i
+    end
+
+    calibration_values.sum
   end
 
   def second_part
