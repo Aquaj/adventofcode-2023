d0848d20f376a267e18e02f1123030dfb93a9f0b
Jérémie <jeremie.bonal@gmail.com>
2023-12-01 11:48:21 +0100

    Part 2

--- day-01.rb
+++ day-01.rb
@@ -13,12 +13,35 @@ class Day1 < AdventDay
   end
 
   def second_part
+    calibration_values = input.map do |line|
+      digits = extract_digits(line, spelled_out: true)
+      [digits.first, digits.last].join.to_i
+    end
+
+    calibration_values.sum
   end
 
   private
 
-  def extract_digits(line)
-    line.gsub(/\D/, '').chars
+  DIGIT_WORDS = %w[one two three four five six seven eight nine].freeze
+
+  def extract_digits(line, spelled_out: false)
+    matches = [/\d/]
+    matches += DIGIT_WORDS if spelled_out
+
+    # By using a lookahead *with* a capture group, we can have a regexp that'll
+    # get overlaps. e.g.: oneight => one, eight
+    overlapping_scan_regex = /(?=(#{matches.join('|')}))/
+
+    line.scan(overlapping_scan_regex).map do |(number_or_word)|
+      if spelled_out && DIGIT_WORDS.include?(number_or_word)
+        number_or_word => word
+        DIGIT_WORDS.index(word) + 1 # + 1 to account for lack of `zero` in DIGIT_WORDS
+      else
+        number_or_word => number
+        number.to_i
+      end
+    end
   end
 
   def convert_data(data)
