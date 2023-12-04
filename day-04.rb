8d1b4f361fa0aaa32fb1a54c9dc490c506f10d0e
Jérémie <jeremie.bonal@gmail.com>
2023-12-04 11:00:16 +0100

    Part 1

--- day-04.rb
+++ day-04.rb
@@ -1,7 +1,13 @@
 require_relative 'common'
 
 class Day4 < AdventDay
+  EXPECTED_RESULTS = { 1 => 13 }
+
   def first_part
+    wins = cards.map do |card|
+      (card[:winners] & card[:numbers]).count
+    end
+    wins.sum { |win_count| win_count.zero? ? 0 : 2**(win_count-1) }
   end
 
   def second_part
@@ -9,8 +15,14 @@ class Day4 < AdventDay
 
   private
 
+  alias cards input
+
+  CARD_FORMAT = /^Card *(\d+): ((?: *\d+ ?)+) \| ((?: *\d+ ?)+)$/
   def convert_data(data)
-    super
+    super.map do |card|
+      id, winners, numbers = card.match(CARD_FORMAT).captures
+      { id:, winners: winners.split.map(&:to_i), numbers: numbers.split.map(&:to_i) }
+    end
   end
 end
 
